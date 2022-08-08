31c8c29ec9daa532b8cbb575751d1f7ec504c64e
Edward Beesley <edwardjbeesley@gmail.com>
2022-08-08 17:15:22 +0100

    completed all challenges

---
+++ 07 - Array Cardio Day 2/index.js
@@ -0,0 +1,54 @@
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 }
+];
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+const overNineteen = people.some((person) => {
+  currentYear = new Date().getFullYear();
+  return (person.year + 19) < currentYear;
+});
+
+console.log(overNineteen);
+// Array.prototype.every() // is everyone 19 or older?
+const allOverNineteen = people.every((person) => {
+  currentYear = new Date().getFullYear();
+  return (person.year + 19) < currentYear;
+});
+
+console.log(allOverNineteen);
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+const rightComment = comments.find((comment) => {
+  return comment.id === 823423;
+});
+
+console.log(rightComment);
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+
+const findComment = comments.findIndex((comment) => {
+  return comment.id === 823423;
+});
+
+console.log(findComment);
+
+comments.splice(findComment, 1);
+
+console.log(comments);
