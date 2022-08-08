318fc8eb87fc22af780cfc41da4bd32a28b1d314
Edward Beesley <edwardjbeesley@gmail.com>
2022-08-08 16:37:25 +0100

    matching input now highlights on the results

--- 06 - Type Ahead/index.js
+++ 06 - Type Ahead/index.js
@@ -1,5 +1,7 @@
 const searchBar = document.querySelector(".search");
 
+const list = document.querySelector(".suggestions");
+
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
 const cities = [];
@@ -13,8 +15,22 @@ fetch(endpoint)
 searchBar.addEventListener('keyup', update);
 
 function update() {
-  getResults(searchBar.value, cities)
-}
+  const results = getResults(searchBar.value, cities);
+  let html = results.map(city => {
+    const regex = new RegExp(this.value, 'gi');
+    const cityName = city.city.replace(regex, `<span class="hl">${this.value}</span>`);
+    const stateName = city.state.replace(regex, `<span class="hl">${this.value}</span>`);
+    return `<li>
+      <span>${cityName}, ${stateName}</span>
+      <span>${city.population}</span>
+    </li>`;
+  }).join('');
+  if (searchBar.value === "") {
+    html = `<li>Filter for a city</li>
+    <li>or a state</li>`
+  };
+  list.innerHTML = html;
+};
 
 function getResults(searchText, cities) {
   return cities.filter((city) => {
